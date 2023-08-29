ed3d0a3ca2cdbad99f9383127fe46c0b3f106867
Lucas Liborio <lucasl.troncoso@gmail.com>
2023-08-29 18:45:15 -0300

    feat: implementing ArrayValidation controller and test

--- src/array-validation/array-validation.controller.spec.ts
+++ src/array-validation/array-validation.controller.spec.ts
@@ -1,20 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArrayValidationController } from './array-validation.controller';
 import { ArrayValidationService } from './array-validation.service';
-
+import { faker } from '@faker-js/faker'
+import { ArrayValidationDto } from './dto/array-validation.dto';
 describe('ArrayValidationController', () => {
   let controller: ArrayValidationController;
+  let service: ArrayValidationService;
+
+  const ArrayValidationStub = {
+    provide: ArrayValidationService,
+    useFactory: () => ({
+      validate: jest.fn(() => [])
+    })
+  }
+  const MockArrayValidationDto = (): string => {
+    return faker.string.alphanumeric({ length: 15 })
+
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ArrayValidationController],
-      providers: [ArrayValidationService],
+      providers: [ArrayValidationStub],
     }).compile();
 
     controller = module.get<ArrayValidationController>(ArrayValidationController);
+    service = module.get<ArrayValidationService>(ArrayValidationService)
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+
+  it('Should call validate())', () => {
+    const serviceSpy = jest.spyOn(service, 'validate')
+    const mockArrayValidationDto = MockArrayValidationDto()
+    controller.create({data: mockArrayValidationDto})
+    expect(serviceSpy).toBeCalled();
+  });
+  
+  it('Should call validate() with correct value)', () => {
+    const serviceSpy = jest.spyOn(service, 'validate')
+    const mockArrayValidationDto = MockArrayValidationDto()
+    controller.create({data: mockArrayValidationDto})
+    expect(serviceSpy).toBeCalledWith(mockArrayValidationDto)
+  });
+  
+  it('Should result with correct value', () => {
+    jest.spyOn(service, 'validate').mockReturnValue({
+      numbers: [1, 2, 3, 4, 5, 6, 7],
+      letters: ["a", "b", "c", "d", "e", "f", "g"]
+    })
+    const result = controller.create({ data: 'abc123defg4567' })
+    expect(result).toStrictEqual({
+      numbers: [1, 2, 3, 4, 5, 6, 7],
+      letters: ["a", "b", "c", "d", "e", "f", "g"]
+    })
   });
 });
--- src/array-validation/array-validation.controller.ts
+++ src/array-validation/array-validation.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { ArrayValidationService } from './array-validation.service';
 import { ArrayValidationDto } from './dto/array-validation.dto';
 
 
 @Controller('array-validation')
 export class ArrayValidationController {
-  constructor(private readonly arrayValidationService: ArrayValidationService) {}
+  constructor(private readonly arrayValidationService: ArrayValidationService) { }
 
   @Post('/validate')
-  create(@Body() arrayValidationDto: ArrayValidationDto) {
-    return this.arrayValidationService.validate(arrayValidationDto);
+  create(@Body(new ValidationPipe()) arrayValidationDto: ArrayValidationDto) {
+    const { data } = arrayValidationDto
+    return this.arrayValidationService.validate(data);
   }
 
 }
