9600e9658f42b800b0a0c9272592ce77c7e3335b
Lucas Liborio <lucasl.troncoso@gmail.com>
2023-08-29 21:28:03 -0300

    fix: fix messafe for StringCheckDto

--- src/string-check/dto/string-check.dto.ts
+++ src/string-check/dto/string-check.dto.ts
@@ -2,7 +2,7 @@ import { Matches } from "class-validator";
 
 export class StringCheckDto {
   @Matches("^[a-zA-Z0-9]+$", 'g', {
-    message: 'data must be an array with only letters and numbers'
+    message: 'data must be a string with only letters and numbers'
   })
   data: string
 }
